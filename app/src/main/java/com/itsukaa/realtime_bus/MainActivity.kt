fc440415e50914cef990bdb62fb7fd61c2b038e5
itsukaa <itsukaa@foxmail.com>
2021-04-19 15:02:13 +0800

    创建项目

---
+++ app/src/main/java/com/itsukaa/realtime_bus/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.itsukaa.realtime_bus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
