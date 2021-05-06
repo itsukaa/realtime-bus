7ec3a614d8dea07d589e20d407678747a39eb7d7
itsukaa <itsukaa@foxmail.com>
2021-05-06 11:57:49 +0800

    五一假期休息，还有两天时间，抓紧时间做 测试了高德地图的搜索公交的功能，通过我的测试，可以实现以下几个功能。 1、 通过经纬度搜索附近的站台，以及站台的详细信息。 2、 通过站台搜索该站台的线路，得到线路id。 3、 通过线路id可以的到线路的完整路程，也就是折线图，可以在地图上画出来。

--- app/src/main/java/com/itsukaa/realtime_bus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/itsukaa/realtime_bus/ui/home/HomeFragment.kt
@@ -7,10 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.amap.api.services.busline.BusLineQuery
+import com.amap.api.services.busline.BusLineSearch
+import com.amap.api.services.busline.BusStationQuery
+import com.amap.api.services.busline.BusStationSearch
+import com.amap.api.services.core.LatLonPoint
+import com.amap.api.services.core.PoiItem
+import com.amap.api.services.poisearch.PoiResult
+import com.amap.api.services.poisearch.PoiSearch
 import com.itsukaa.realtime_bus.R
 import com.itsukaa.realtime_bus.data.adapter.StationsAdapter
 import com.itsukaa.realtime_bus.data.entity.Line
 import com.itsukaa.realtime_bus.data.entity.Station
+import com.orhanobut.logger.Logger
+
 
 /**
  * A simple [Fragment] subclass.
@@ -38,6 +48,7 @@ class HomeFragment : Fragment() {
 
     fun initData() {
 
+
         val line804 = Line("804", "南湖路江南村")
         val line805 = Line("805", "和平大道杨园")
         val line806 = Line("806", "彭刘杨路")
@@ -59,7 +70,57 @@ class HomeFragment : Fragment() {
 
         stations = listOf(station1, station2, station3, station5, station4)
 
-    }
+        //"150700"
+        val query = PoiSearch.Query("", "150700", "武汉")
+        query.pageSize = 10
+        query.pageNum = 0
+        query.cityLimit = true
 
+        val poiSearch = PoiSearch(context, query)
+        var busStations = ArrayList<String>()
+
+        poiSearch.setOnPoiSearchListener(object : PoiSearch.OnPoiSearchListener {
+            override fun onPoiSearched(result: PoiResult?, rCode: Int) {
+                for (poiItem in result!!.pois) {
+                    busStations.add(poiItem.title)
+                }
+                for (busStation in busStations) {
+                    val busStationQuery = BusStationQuery(busStation, "武汉")
+                    val busStationSearch = BusStationSearch(context, busStationQuery)
+                    busStationSearch.setOnBusStationSearchListener { busStationResult, _ ->
+                        for (item in busStationResult.busStations) {
+                            for (busLineItem in item.busLineItems) {
+                                val busLineQuery = BusLineQuery(
+                                    busLineItem.busLineId,
+                                    BusLineQuery.SearchType.BY_LINE_ID,
+                                    "武汉"
+                                )
+                                busLineQuery.pageSize = 10
+                                busLineQuery.pageNumber = 0
+
+                                Logger.w(busLineItem.busLineId)
+                                val busLineSearch = BusLineSearch(context, busLineQuery)
+                                busLineSearch.setOnBusLineSearchListener { busLineResult, _ ->
+                                    for (busLine in busLineResult.busLines) {
+                                        Logger.w(busLine.directionsCoordinates.toString())
+                                    }
+                                }
+                                busLineSearch.searchBusLineAsyn()
+                            }
+                        }
+                    }
+                    busStationSearch.searchBusStationAsyn()
+                }
+
+            }
+
+            override fun onPoiItemSearched(p0: PoiItem?, p1: Int) {
+                TODO("Not yet implemented")
+            }
+        })
+        poiSearch.searchPOIAsyn()
+        poiSearch.bound = PoiSearch.SearchBound(LatLonPoint(30.555537, 114.314724), 500)
+        Logger.w("发送请求")
+    }
 
 }
